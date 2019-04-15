ee922ce74d8c3731de2d999ec15847f34b38dcca
IMSindhu <49526967+IMSindhu@users.noreply.github.com>
2019-04-15 12:21:11 +0530

    Add files via upload

---
+++ Posts-MVC-master/main/java/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+public class User {
+	
+	private int user_id;
+	private String user_name;
+	private String name;
+	private String email_id;
+	private String password;
+	
+	
+
+	
+}
